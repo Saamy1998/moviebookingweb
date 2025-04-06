d13b865f051495a0140d9b7d9b4a3a828008e90f
Saamy1998 <110920543+Saamy1998@users.noreply.github.com>
2025-04-07 00:29:39 +0530

    Update auth.js

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://moviebookingweb-ifqr.onrender.com",
 });
 
 export const registerUser = (formData) => API.post("/auth/register", formData);
